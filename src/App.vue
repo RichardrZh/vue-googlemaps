078d8afdddb1c68bf699e88099cc648d337fa4c8
Richard Zhuang <richard.zhuang91@gmail.com>
2023-10-28 10:02:55 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import LeftSidebar from './components/LeftSidebar.vue'
+import MapPanel from './components/MapPanel.vue'
+</script>
+
+<template>
+
+  <main class="flex items-stretch w-screen max-w-full overflow-x-hidden">
+    
+    <LeftSidebar />
+
+    <MapPanel /> 
+
+  </main>
+
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,37 @@
+import './styles/main.css'
+import { GOOGLE_MAPS_API_KEY } from './config.json'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/* pinia */
+import { createPinia } from 'pinia'
+
+/* fontawesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import { faLocationDot, faSearch } from '@fortawesome/free-solid-svg-icons'
+
+/* vue google maps */
+import VueGoogleMaps from 'vue-google-maps-community-fork'
+
+const app = createApp(App)
+
+/* fontawesome */
+library.add(faLocationDot, faSearch);
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+/* pinia */
+const pinia = createPinia()
+app.use(pinia)
+
+/* vue 3 google maps */
+app.use(VueGoogleMaps, {
+    load: {
+        key: GOOGLE_MAPS_API_KEY,
+        libraries: 'places',
+    },
+})
+
+app.mount('#app')
