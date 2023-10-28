d94c983a677c73dafd5572faf9af53cfda854061
Richard Zhuang <richard.zhuang91@gmail.com>
2023-10-28 10:13:08 -0400

    feat: created MapPanel template; added styles

--- src/components/MapPanel.vue
+++ src/components/MapPanel.vue
@@ -1,13 +1,34 @@
 <script setup>
-
+import GoogleMaps from './GoogleMaps.vue'
+import Searchbar from './Searchbar.vue'
 
 </script>
 
 <template>
+  <section class="h-screen max-h-full flex-grow z-10 relative">
 
+    <!-- search field from maps api, maybe wrap it and add buttons: 
+      search(mag glass), curr loc icons(house?) -->
+    <div class="absolute z-10 searchbar-wrapper">
+      <Searchbar />
+    </div>
+    
+    <!-- google maps map from api -->
+    <div class="w-full h-full z-0">
+      <GoogleMaps  />
+    </div>
+
+  </section>
 </template>
 
 <style scoped>
+section {
+  background-color: lightblue;
+}
 
-
+.searchbar-wrapper {
+  top: 16px;
+  left: 50%;
+  transform: translate(-50%);
+}
 </style>
