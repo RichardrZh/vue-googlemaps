427f668a4a9f77662f1245490b8a0f1626ce3524
Richard Zhuang <richard.zhuang91@gmail.com>
2023-10-29 23:34:08 -0400

    refact: created lib to handle datetime formatting

--- src/scripts/DateTimeUtils.js
+++ src/scripts/DateTimeUtils.js
@@ -1,16 +1,28 @@
 
-function getDateString(timestamp, options) {
-    let date = new Date(timestamp)
-
-    if (options) {
-        var time = new Intl.DateTimeFormat(navigator.language, options).format(date)
+function getDayName(date, options) {
+    let day = date.getDay()
+    if (options == "short") {
+        let shortDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        return shortDays[day]
     } else {
-        var time = new Intl.DateTimeFormat(navigator.language).format(date)
+        let longdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        return longdays[day]
     }
-
-    return time
 }
 
+function getFormattedTime(localTimestamp) {
+    let date = new Date(localTimestamp)
+    let hours = date.getUTCHours()
+    let minutes = date.getUTCMinutes()
+    let ampm = hours >= 12 ? "pm" : "am"
+    hours = hours % 12
+    hours = hours ? hours : 12
+    minutes = minutes < 10 ? `0${minutes}` : minutes
+    return `${hours}:${minutes} ${ampm}`
+}
+
+
 export {
-    getDateString
+    getDayName,
+    getFormattedTime
 }
